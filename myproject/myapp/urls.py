1627d79ef04580239c6917c330295249d864417a
Aliaksandra Khvaliuk <a.hvaluk@gmail.com>
2025-03-19 17:00:59 +0100

    Migrations

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),  
-    path('aboutus/', views.about, name="aboutus"),
-    path('menu/', views.menu, name="menu"),
-    path('book/', views.book, name="book"),
+    
 ]
